200b45f3b9b3c5fe809ca878e011fd135a83de35
Henry Jung <butwoo91@gmail.com>
2025-05-29 14:20:21 +1000

    configuring

--- config/initializers/cloth_types.rb
+++ config/initializers/cloth_types.rb
@@ -1,4 +1,4 @@
 CLOTH_TYPES = (ENV['CLOTH_TYPES'] ? ENV['CLOTH_TYPES'].split(',').map(&:strip) : %w[
   Jeans Swimsuit Blouse Cardigan Dress Skirt Bodysuit Halterneck
-  Jacket Shorts T-shirt Boot Hat Hoodie Sweater Vest Bathrobe Pants Polo\ shirt Shirt
+  Jacket Shorts T-shirt Boot Hat Hoodie Sweater Vest Bathrobe Pants PoloShirt Shirt
 ]).freeze
